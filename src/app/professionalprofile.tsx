bd5a06078ce52add452eb4a946db797f8d4aba99
Ariadne Coelho <ariadnecoelho@alu.ufc.br>
2025-02-23 23:40:04 +0000

    Adiciona header com ícones de voltar, logo e perfil

--- src/app/professionalprofile.tsx
+++ src/app/professionalprofile.tsx
@@ -1,9 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const ProfessionalProfile = () => {
   return (
     <View style={styles.container}>
-      <Text>Professional Profile</Text>
+      {/* Header */}
+      <View style={styles.header}>
+        <TouchableOpacity style={styles.backButton}>
+          <Image source={require('../assets/arrow_back.png')} style={styles.backButton} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={require('../assets/logo_blue.png')} style={styles.headerLogo} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={require('../assets/profile-icon.png')} style={styles.profileIcon} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,6 +23,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f2f2f2',
+  },
+  header: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 60,
+    paddingHorizontal: 10,
+    backgroundColor: '#e9501a',
+  },
+  headerLogo: {
+    width: 120,
+    height: 40,
+    resizeMode: 'contain',
+  },
+  backButton: {
+    height: 30,
+    width: 30,
+  },
+  profileIcon: {
+    height: 50,
+    width: 50,
   },
 });
 
